03e8eecd78345afce078d2fb51ce84c4668cda60
gennady-ogg <genn.osipov2011@yandex.ru>
2022-01-03 23:43:50 +0300

    world

---
+++ app/js/main.js
@@ -0,0 +1,5 @@
+(()=>{var __webpack_modules__={"./src/js/main.js":
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/()=>{eval("window.onload = function () {\n  document.querySelector('.preloader').classList.add(\"preloader-remove\");\n};\n\n//# sourceURL=webpack://coca-cola/./src/js/main.js?")}},__webpack_exports__={};__webpack_modules__["./src/js/main.js"]()})();
+//# sourceMappingURL=main.js.map
